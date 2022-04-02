842586b95f09132d91451369f39fa169950fdb81
Alan Jones <jones.alan@hotmail.co.uk>
2022-04-02 18:17:09 +0200

    update layout

---
+++ templates/makepage4.py
@@ -0,0 +1,21 @@
+HTML_FILE_NAME ='index.html'
+TEMPLATE = 'index4template.html'
+TEMPLATE_DIR = 'templates'
+ARTICLES_FILE = 'pages.json'
+
+from jinja2 import Environment, FileSystemLoader
+import json
+
+pagefile = open(ARTICLES_FILE,'r')
+pages = json.load(pagefile)
+pagefile.close()
+
+file_loader = FileSystemLoader(TEMPLATE_DIR)
+env = Environment(loader=file_loader)
+template = env.get_template(TEMPLATE)
+
+output = template.render(pages=pages)
+
+file = open(HTML_FILE_NAME,'w')
+file.write(output)
+file.close()
